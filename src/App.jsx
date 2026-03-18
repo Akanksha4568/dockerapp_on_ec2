b3966e72fdec9a8ffd60d4efed75f0a41f240461
Akanksha <akanksha4644@gmail.com>
2026-03-18 18:21:16 +0530

    second

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <form className='form' centered="true">
+        <label htmlFor="name">Name</label>
+        <input type='text' id='name' name='name'></input>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
